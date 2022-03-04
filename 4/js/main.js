f697e2d9199e096a9a2d2a4e450adda2d8676ddc
Keks <keks@htmlacademy.ru>
2022-03-04 13:27:25 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,17 @@
+// text
+// import {getRandomFloat, getRandomInteger, getUnicRangomArray, getNonUnicRangomArray} from './utils/utils.js';
+// import { getOfferTitle, getOfferPlace, getCheckinTime, getCheckoutTime, getFeatures, getDescriptions, getPhotos } from './config.js';
+
+import { prepareTestData } from './utils/create-test-data.js';
+
+import './map/map-config.js'; // конфигурация  для карты
+import './map/map.js'; //згрузка и инициализация карты
+import './map/map-filter.js'; // работа с фильтром карты
+import './map/map-popup.js'; // всплывающая карточка на карте
+import './form/check-form.js'; // проверка полей формы
+import './form/form.js'; // работа с формой
+import './alert-popup.js'; // попапы для ошибок
+import './server.js'; // взаимодействие с сервером
+
+prepareTestData();
+// console.log(prepareTestData());
