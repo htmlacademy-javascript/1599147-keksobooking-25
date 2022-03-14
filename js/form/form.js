f06eab3ea071c5bea4a7305ba5d623c15e920a91
dimpulsha <dimpulsha@gmail.com>
2022-03-14 17:00:35 +0500

    disable offer form function

---
+++ js/form/form.js
@@ -0,0 +1,12 @@
+//модуль работы с формой
+const offerForm = document.querySelector('.ad-form');
+const filterForm = document.querySelector('.ad-form--disabled');
+
+const disableOfferForm = () => {
+  offerForm.classList.add('ad-form--disabled');
+console.log('disable offerForm');
+};
+
+disableOfferForm();
+
+export { disableOfferForm };
